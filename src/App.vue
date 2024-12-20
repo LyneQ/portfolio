896d90c1721bf6b506ae1469f6ac95404bbaca17
lynhe <lynhedev@gmail.com>
2024-12-20 21:12:14 +0100

    feat(header): added burger menu

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <script setup lang="ts">
 import Card from "@/components/card.vue";
 import Project from "@/components/project.vue";
+// import burgerIcon from "@/assets/burger.svg";
+
+import {ref, watch} from "vue";
+const isMenuOpen = ref(false);
+const isDesktop = ref(window.innerWidth > 510);
+
+const handleBurgerClick = () => {
+  console.log("clicked", isMenuOpen.value);
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+import { onMounted, onUnmounted } from "vue";
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 510;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", updateWidth);
+  updateWidth();
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateWidth);
+});
+
 </script>
 <template>
   <header>
     <div class="wrapper">
       <h1 class="name">Lyne Q.</h1>
-      <nav>
+      <nav v-show=" isMenuOpen || isDesktop ">
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#experience">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
+      <img class="burgerButton" src="/burger.svg" alt="Menu" draggable="false" :onclick="handleBurgerClick"/>
     </div>
   </header>
 
@@ -87,6 +114,7 @@ import Project from "@/components/project.vue";
 </template>
 
 <style scoped>
+
 .subTitle {
   text-align: center;
   font-size: 2.5rem;
@@ -113,21 +141,40 @@ header {
     /* padding: 1rem; */
     padding: 0 15vw;
 
-    @media (max-width: 480px) {
-      padding: 0;
+    .burgerButton {
+      display: none;
+      font-size: 2rem;
+      padding: 0 1rem;
+      background: none;
+      border: none;
+      cursor: pointer;
+
+      @media screen and (max-width: 510px) {
+        display: block;
+      }
     }
 
     ul {
       display: flex;
       list-style: none;
       gap: 1rem;
+
+      @media screen and (max-width: 510px) {
+        flex-direction: column;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        background-color: var(--color-surface);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        padding: 1rem;
+        gap: 1rem;
+      }
     }
 
     .name {
-      /*@media (max-width: 480px) {
-      //  padding-left: 0;
-      //}
-      //padding-left: 15vw;*/
       font-size: 2rem;
 
       background: linear-gradient(45deg, var(--color-accent), var(--color-accent), var(--color-accent), var(--color-secondary));
