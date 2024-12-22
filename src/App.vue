eadd8b0c023f0fa07c3f08ae6876984452bae9a3
lynhe <lynhedev@gmail.com>
2024-12-22 02:05:53 +0100

    refactor(header): separate header in

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <script setup lang="ts">
 import Card from "@/components/card.vue";
 import Project from "@/components/project.vue";
+import Navigation from "@/components/Navigation.vue";
 // import burgerIcon from "@/assets/burger.svg";
 
-import {ref, watch} from "vue";
-const isMenuOpen = ref(false);
-const isDesktop = ref(window.innerWidth > 510);
-
-const handleBurgerClick = () => {
-  console.log("clicked", isMenuOpen.value);
-  isMenuOpen.value = !isMenuOpen.value;
-};
-
-import { onMounted, onUnmounted } from "vue";
-
-const updateWidth = () => {
-  isDesktop.value = window.innerWidth > 510;
-};
-
-onMounted(() => {
-  window.addEventListener("resize", updateWidth);
-  updateWidth();
-});
-
-onUnmounted(() => {
-  window.removeEventListener("resize", updateWidth);
-});
 
 </script>
 <template>
-  <header>
-    <div class="wrapper">
-      <h1 class="name">Lyne Q.</h1>
-      <nav v-show=" isMenuOpen || isDesktop ">
-        <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <img class="burgerButton" src="/burger.svg" alt="Menu" draggable="false" :onclick="handleBurgerClick"/>
-    </div>
-  </header>
 
+<Navigation>
+</Navigation>
   <main id="about">
     <div class="container">
       <img class="avatar" src="/lyneq2.png" alt="Illustration of me"/>
@@ -125,63 +92,6 @@ onUnmounted(() => {
   background: linear-gradient(45deg, var(--color-accent), var(--color-accent), var(--color-accent), var(--color-secondary));
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-}
-
-header {
-  background-color: var(--color-surface);
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-  position: fixed;
-  width: 100%;
-
-  .wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    /* padding: 1rem; */
-    padding: 0 15vw;
-
-    .burgerButton {
-      display: none;
-      font-size: 2rem;
-      padding: 0 1rem;
-      background: none;
-      border: none;
-      cursor: pointer;
-
-      @media screen and (max-width: 510px) {
-        display: block;
-      }
-    }
-
-    ul {
-      display: flex;
-      list-style: none;
-      gap: 1rem;
-
-      @media screen and (max-width: 510px) {
-        flex-direction: column;
-        position: absolute;
-        display: flex;
-        align-items: center;
-        top: 100%;
-        left: 0;
-        width: 100%;
-        background-color: var(--color-surface);
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        padding: 1rem;
-        gap: 1rem;
-      }
-    }
-
-    .name {
-      font-size: 2rem;
-
-      background: linear-gradient(45deg, var(--color-accent), var(--color-accent), var(--color-accent), var(--color-secondary));
-      -webkit-background-clip: text;
-      -webkit-text-fill-color: transparent;
-    }
-  }
 }
 
 main {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import {ref, watch} from "vue";
+const isMenuOpen = ref(false);
+const isDesktop = ref(window.innerWidth > 510);
+
+const handleBurgerClick = () => {
+  console.log("clicked", isMenuOpen.value);
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+import { onMounted, onUnmounted } from "vue";
+
+const updateWidth = () => {
+  isDesktop.value = window.innerWidth > 510;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", updateWidth);
+  updateWidth();
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateWidth);
+});
+
+</script>
+
+<template>
+  <section>
+    <header>
+      <div class="wrapper">
+        <h1 class="name">Lyne Q.</h1>
+        <nav v-show=" isMenuOpen || isDesktop ">
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <img class="burgerButton" src="/burger.svg" alt="Menu" draggable="false" v-bind:onclick="handleBurgerClick"/>
+      </div>
+    </header>
+    {{ $slots }}
+  </section>
+
+</template>
+
+<style scoped>
+header {
+  background-color: var(--color-surface);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  position: fixed;
+  width: 100%;
+
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* padding: 1rem; */
+    padding: 0 15vw;
+
+    .burgerButton {
+      display: none;
+      font-size: 2rem;
+      padding: 0 1rem;
+      background: none;
+      border: none;
+      cursor: pointer;
+
+      @media screen and (max-width: 510px) {
+        display: block;
+      }
+    }
+
+    ul {
+      display: flex;
+      list-style: none;
+      gap: 1rem;
+
+      @media screen and (max-width: 510px) {
+        flex-direction: column;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        background-color: var(--color-surface);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        padding: 1rem;
+        gap: 1rem;
+      }
+    }
+
+    .name {
+      font-size: 2rem;
+
+      background: linear-gradient(45deg, var(--color-accent), var(--color-accent), var(--color-accent), var(--color-secondary));
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+}
+</style>
